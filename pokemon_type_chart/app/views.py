e8ec29b4b82e248046d4cb138d0eafe514b4f56d
ematias10 <ematiasal9@gmail.com>
2023-11-01 23:49:27 -0300

    segundo commit

--- pokemon_type_chart/app/views.py
+++ pokemon_type_chart/app/views.py
@@ -1,3 +1,10 @@
+
+
+from django.template import loader
+from django.http import HttpResponse, JsonResponse
 from django.shortcuts import render
 
-# Create your views here.
+def indexfw2(request):	
+	template = loader.get_template()
+	context = {}
+	return HttpResponse(template.render(context, request))
